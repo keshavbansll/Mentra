66ceeb2fb2b567e6ec5adec0990836be7379a6d7
Keshav Bansal <officialkeshavbansal@gmail.com>
2025-08-06 08:46:05 +0530

    Search categories and actions added. Dictionary API integrated

--- js/main.js
+++ js/main.js
@@ -15,3 +15,63 @@ function getFormattedDay() {
 
 date.textContent = getFormattedDate();
 day.textContent = getFormattedDay();
+
+const userQuery = document.querySelector("#search");
+const selectedSearchType = document.querySelector("#category");
+
+userQuery.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    search();
+  }
+});
+
+function search() {
+  const query = userQuery.value.trim();
+  const platform = selectedSearchType.value;
+
+  if (!query || platform === "none") {
+    alert("Please enter a search query and select a platform");
+    return;
+  }
+
+  let url = "";
+
+  if (platform == "google") {
+    url = "https://www.google.com/search?q=" + query;
+  }
+
+  if (platform == "youtube") {
+    url = "https://www.youtube.com/results?search_query=" + query;
+  }
+
+  if (platform == "mdn") {
+    url = "https://developer.mozilla.org/en-US/search?q=" + query;
+  }
+
+  if (platform == "wiki") {
+    url = "https://en.wikipedia.org/wiki/Special:Search?search=" + query;
+  }
+
+  if (url) {
+    window.open(url, "_blank");
+  }
+
+  if (platform == "dict") {
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + query;
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(`Word: ${response.data[0].word}`);
+        console.log(`Pronunciation: ${response.data[0].phonetics[0].text}`);
+        console.log(`Adjective: ${response.data[0].meanings[0].partOfSpeech}`);
+        console.log(
+          `Definition: ${response.data[0].meanings[0].definitions[0].definition}`
+        );
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log("Error Fetching Data:", e);
+      });
+  }
+}
